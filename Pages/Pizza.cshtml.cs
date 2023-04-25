54e249f8c49ef488fa16da5215f2929d700c092a
Michle99 <sammiyonyx@gmail.com>
2023-04-25 17:33:54 -0400

    first commit

---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
+
+
+namespace RazorPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+{
+    public List<Pizza> pizzas = new();
+    [BindProperty]
+    public Pizza NewPizza { get; set; } = new();
+
+    public void OnGet()
+    {
+        pizzas = PizzaService.GetAll();
+    }
+    
+    public string GlutenFreeText(Pizza pizza)
+    {
+        return pizza.IsGlutenFree ? "Gluten Free": "Not Gluten Free";
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        PizzaService.Add(NewPizza);
+        return RedirectToAction("Get");
+    }
+
+    public IActionResult OnPostDelete(int id)
+    {
+        PizzaService.Delete(id);
+        return RedirectToAction("Get");
+    }
+}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Create a web UI with ASP.NET Core 
+
+# Description
+Create a web UI with ASP.NET Core using Razor Pages.
+
+# Resources
+[Create a web UI with ASP.NET Core](https://learn.microsoft.com/en-us/training/modules/create-razor-pages-aspnet-core/)
